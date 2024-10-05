dbea6b6b4f862403afcaf15dfdea6ca1843a2e4a
Patryk <patryknartowski@wp.pl>
2024-10-05 18:23:21 +0200

    prepare database

--- src/test/java/org/example/carrent/CarRentalApplicationTests.java
+++ src/test/java/org/example/carrent/CarRentalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarRentApplicationTests {
+class CarRentalApplicationTests {
 
 	@Test
 	void contextLoads() {
