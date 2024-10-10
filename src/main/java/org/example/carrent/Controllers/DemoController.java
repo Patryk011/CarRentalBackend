af8a90940cd430c6f24c2a5b686e3d66e45c2c81
Patryk <patryknartowski@wp.pl>
2024-10-10 23:55:00 +0200

    config

--- src/main/java/org/example/carrent/Controllers/DemoController.java
+++ src/main/java/org/example/carrent/Controllers/DemoController.java
@@ -1,20 +1,21 @@
 package org.example.carrent.Controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class IndexController {
+public class DemoController {
 
     @GetMapping("/public")
-    public String publicEndpoint() {
-        return "This is a public endpoint!";
+    public String hello() {
+        return "Hello Public";
     }
 
     @GetMapping("/private")
-    public String privateEndpoint() {
-        return "This is a secured endpoint. You must be authenticated to see this!";
+    public String hello2() {
+        return "Hello Private";
     }
 }
--- src/main/java/org/example/carrent/Security/SecurityConfig.java
+++ src/main/java/org/example/carrent/Security/SecurityConfig.java
@@ -3,11 +3,13 @@ package org.example.carrent.Security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -22,10 +24,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/public").permitAll().anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(null)))
+                        .jwt(withDefaults()))
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(STATELESS)
