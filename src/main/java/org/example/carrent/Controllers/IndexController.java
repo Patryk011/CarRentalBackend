803e1fffdfc69b0a66aca02c8d4a996ba2d427f1
Patryk <patryknartowski@wp.pl>
2024-10-10 22:21:28 +0200

    config

--- src/main/java/org/example/carrent/Controllers/IndexController.java
+++ src/main/java/org/example/carrent/Controllers/IndexController.java
@@ -1,4 +1,20 @@
 package org.example.carrent.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class IndexController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "This is a public endpoint!";
+    }
+
+    @GetMapping("/private")
+    public String privateEndpoint() {
+        return "This is a secured endpoint. You must be authenticated to see this!";
+    }
 }
--- src/main/java/org/example/carrent/Security/SecurityConfig.java
+++ src/main/java/org/example/carrent/Security/SecurityConfig.java
@@ -1,21 +1,36 @@
 package org.example.carrent.Security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(null)))
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS)
+                );
+
         return http.build();
     }
 }
