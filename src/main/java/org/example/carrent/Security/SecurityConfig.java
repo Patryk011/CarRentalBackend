7db5f294900da0845388b2e070459d68a2f2cfa2
Patryk011 <96496498+Patryk011@users.noreply.github.com>
2024-10-17 01:23:37 +0200

    Stage (#4)
    * config
    
    * config
    
    * add correct authorization
    
    * adjust config
    
    * add registration
    
    * update
    
    * fb
    
    * update
    
    * add google login
    
    * styles
    
    * update
    
    * update
    
    * update

--- src/main/java/org/example/carrent/Security/SecurityConfig.java
+++ src/main/java/org/example/carrent/Security/SecurityConfig.java
@@ -1,21 +1,38 @@
 package org.example.carrent.Security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public").permitAll().anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults()))
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS)
+                );
+
         return http.build();
     }
 }
