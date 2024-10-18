1104cb81260545b66e3fb939e15cf1f7ebc8ad02
Patryk011 <96496498+Patryk011@users.noreply.github.com>
2024-10-18 22:49:04 +0200

    create entity and adjust config (#5)

---
+++ src/main/java/org/example/carrent/Entity/Customer.java
@@ -0,0 +1,47 @@
+package org.example.carrent.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.Date;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+
+
+
+    @Column(name = "license_number", unique = true)
+    private String licenseNumber;
+
+    @Column(name = "address")
+    private String address;
+
+
+
+}
