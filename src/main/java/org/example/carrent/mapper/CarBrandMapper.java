fc91584d62b362291de49a82b7d638f36e43d1b7
ola <wieckowicz.aleksandra@gmail.com>
2024-11-30 00:27:26 +0100

    CarBrand service created

--- src/main/java/org/example/carrent/mapper/CarBrandMapper.java
+++ src/main/java/org/example/carrent/mapper/CarBrandMapper.java
@@ -1,8 +1,13 @@
 package org.example.carrent.mapper;
 
 import org.example.carrent.dto.CarBrandDTO;
+import org.example.carrent.dto.CustomerDTO;
 import org.example.carrent.entity.CarBrand;
+import org.example.carrent.entity.Customer;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CarBrandMapper {
@@ -21,6 +26,11 @@ public class CarBrandMapper {
         entity.setBrand(dto.getBrand());
 
         return entity;
+    }
+
+    public static List<CarBrandDTO> toDto(List<CarBrand> carBrands) {
+        return carBrands.stream().map(CarBrandMapper::toDto)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/org/example/carrent/repository/CarBrandRepository.java
+++ src/main/java/org/example/carrent/repository/CarBrandRepository.java
@@ -1,7 +1,9 @@
 package org.example.carrent.repository;
 
+import org.example.carrent.entity.CarBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CarBrandRepository {
+public interface CarBrandRepository extends JpaRepository<CarBrand, Long> {
 }
