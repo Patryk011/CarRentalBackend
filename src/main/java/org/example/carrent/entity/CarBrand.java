3b8dd6dca50d7599b3f3a5f57ec1b08535987164
ola <wieckowicz.aleksandra@gmail.com>
2024-11-29 23:05:48 +0100

    created car_brand entity

---
+++ src/main/java/org/example/carrent/entity/CarBrand.java
@@ -0,0 +1,23 @@
+package org.example.carrent.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "car_brand")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarBrand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_name", nullable = false)
+    private String brandName;
+}
