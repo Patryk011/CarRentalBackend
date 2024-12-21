8dd981e5c534bc85695bae877c7b817c07cd058b
Patryk011 <96496498+Patryk011@users.noreply.github.com>
2024-12-21 16:29:35 +0100

    Feat/customer data pass (#9)
    Data pass from keycloak

---
+++ keycloakListener/src/main/java/keycloak/spi/RegistrationEventListenerProvider.java
@@ -0,0 +1,79 @@
+package keycloak.spi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RegistrationEventListenerProvider implements EventListenerProvider {
+    private final KeycloakSession session;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public RegistrationEventListenerProvider(KeycloakSession session) {
+        this.session = session;
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        if (event.getType() == EventType.REGISTER) {
+            RealmModel realm = session.getContext().getRealm();
+            UserModel user = session.users().getUserById(realm, event.getUserId());
+
+
+            String firstName = user.getFirstName();
+            String lastName = user.getLastName();
+            String email = user.getEmail();
+            String phoneNumber = user.getFirstAttribute("phoneNumber");
+
+            HashMap<String, Object> customerData = new HashMap<>();
+            customerData.put("firstName", firstName);
+            customerData.put("lastName", lastName);
+            customerData.put("email", email);
+            customerData.put("phoneNumber", phoneNumber);
+
+            System.out.println("Customer data: " + customerData);
+
+
+
+            try {
+                String requestBody = objectMapper.writeValueAsString(customerData);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://spring-backend:8081/customer-sync/register")
+                        )
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                        .build();
+
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() != 200) {
+                    System.err.println("Failed to sync customer to Spring app. Status: " + response.statusCode());
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onEvent(org.keycloak.events.admin.AdminEvent adminEvent, boolean includeRepresentation) {
+    }
+
+    @Override
+    public void close() {
+    }
+}
---
+++ keycloakListener/src/main/java/org/example/keycloaklistener/KeycloakListenerApplication.java
@@ -0,0 +1,13 @@
+package org.example.keycloaklistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakListenerApplication.class, args);
+    }
+
+}
