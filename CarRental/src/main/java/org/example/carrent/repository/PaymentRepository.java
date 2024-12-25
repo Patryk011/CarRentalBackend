c196058fbc42f08d4b6e0e33c99422c4a8e39d5d
patryk011 <patryknartowski@wp.pl>
2024-12-25 01:48:09 +0100

    update

---
+++ CarRental/src/main/java/org/example/carrent/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package org.example.carrent.repository;
+
+
+import org.example.carrent.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
